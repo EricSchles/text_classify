2073838bbb55eb401d6ec4016cf77938825957bd
EricSchles <Ericschles@gmail.com>
2015-06-23 16:22:55 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="text_classify",
+      version="0.0",
+      description="a set of text classification packages",
+      author="Eric Schles",
+      packages=["text_classify"]
+  )
